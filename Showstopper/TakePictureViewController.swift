0b585c82f143d7fcdca0eb4bb33d2094831616ee
TheCurryMan <avinashkalpitjain@gmail.com>
2018-01-20 03:14:14 -0800

    ImagePickerController working

--- Showstopper/TakePictureViewController.swift
+++ Showstopper/TakePictureViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class TakePictureCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var clothImageView: UIImageView!
+    @IBOutlet var clothImageView: UIImageView!
+    @IBOutlet var testLabel: UILabel!
+    
 }
 
-class TakePictureViewController: UIViewController {
+class TakePictureViewController: UIViewController, UICollectionViewDataSource, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UICollectionViewDelegate {
 
+    var images = [UIImage(named: "photo-camera")]
+    var imagePicker = UIImagePickerController()
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        UINib(nibName: "TakePictureCollectionViewCell", bundle: nil)
+        self.collectionView.register(UINib(nibName: "TakePictureCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "takephoto")
+        self.view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: self.view.frame, andColors: [HexColor("#ee0979")!, HexColor("#ff6a00")!])
 
         // Do any additional setup after loading the view.
     }
@@ -29,10 +41,40 @@ class TakePictureViewController: UIViewController {
     }
     
     @IBAction func takePhoto(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            images.append(pickedImage)
+            collectionView.reloadData()
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell:TakePictureCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "takephoto", for: indexPath) as! TakePictureCollectionViewCell
+        cell.backgroundColor = UIColor.blue
+        //let imageV = UIImageView(image: images[indexPath.row])
+        cell.clothImageView.image = images[indexPath.row]
+        //cell.testLabel.text = "asd"
+        return cell
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("tapped")
+    }
     /*
     // MARK: - Navigation
 
